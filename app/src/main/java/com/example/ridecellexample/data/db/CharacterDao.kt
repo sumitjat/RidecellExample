68439657b7c9e22fc62e7105f2165c511328cd1b
sumitjat <36793278+sumitjat@users.noreply.github.com>
2022-07-14 09:19:57 +0530

    Update CharacterDao.kt

--- app/src/main/java/com/example/ridecellexample/data/db/CharacterDao.kt
+++ app/src/main/java/com/example/ridecellexample/data/db/CharacterDao.kt
@@ -1,8 +1,6 @@
 package com.example.ridecellexample.data.db
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
@@ -12,15 +10,15 @@ import com.example.ridecellexample.data.model.CharacterData
 interface CharacterDao {
 
     @Insert(onConflict = REPLACE)
-    fun insertCharacters(vararg messageData: CharacterData)
+    suspend fun insertCharacters(vararg messageData: CharacterData)
 
     @Query("SELECT * FROM CharacterData ORDER BY id DESC")
-    fun getAllCharacter(): List<CharacterData>
+    suspend fun getAllCharacter(): List<CharacterData>
 
     @Query("SELECT * FROM CharacterData  WHERE name LIKE :name")
-    fun searchCharacter(name: String): List<CharacterData>
+    suspend fun searchCharacter(name: String): List<CharacterData>
 
     @Query("DELETE FROM CharacterData WHERE  id IN (:id)")
-    fun deleteCharacterData(id: String)
+    suspend fun deleteCharacterData(id: String)
 
 }
